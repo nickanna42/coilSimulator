cf602852d62e916e5c72377dc836081da03b57e7
Nicholas Anna <nickanna_42@yahoo.com>
2016-12-04 19:10:44 -0500

    Further preparation of newCoil.py, in prepartion for it's phase in.

--- newCoil.py
+++ newCoil.py
@@ -14,8 +14,8 @@ def displace(startPoint, endPoint):
     ---------
     Gives the displacement vector between two points.
     """
-    return endPoint - startPoint
-    
+    out = np.array(endPoint - startPoint0, ndmin=2, dtype=float64)
+    return out
 
 def distance(startPoint, endPoint):
     """
@@ -34,21 +34,62 @@ def distance(startPoint, endPoint):
 
 
 
-def b_fromWireSegments(wireMiddle, wireTip, currentVector, evalPoint):
+def b_fromWireSegments(wireMiddle, wireL, current, evalPoint):
     """
-    wireMiddle:      (n,3)np.float64
-    wireTip:         (n,3)np.float64
-    currentVector:   (n,3)np.float64
-    evalPoint:       (1,3)np.float64
+    wireMiddle:    (n,3)np.float64
+    wireL:         (n,3)np.float64
+    current:       (n)np.float64
+    evalPoint:     (1,3)np.float64
 
-    returns: (n,3)np.float64
+    returns: (1,3)np.float64
     -----------
-    This function outputs the magnetic feild at evalPoint of a number
-    of straight wire segments centered at wireMiddle, with the end
-    the current is flowing towards centered at wireTip. The magnitude
-    and 3D orientation in the wire is given by currentVector
+    This function outputs the magnetic feild at evalPoint from a
+    number of straight wire segments. The wires segments are
+    centered at wireMiddle, with current flowing in the direction
+    of the length vector.
     """
-    P_x, P_y, P_z = distance(wireMiddle, evalPoint)
-    I
-    halfL_x, halfL_y, halfL_z = displace(wireMiddle, wireTip)
-    theta
+    N = len(current)
+    constant = np.pi*1e-7*current
+    P = displace(wireMiddle, evalPoint)
+    P_x = P[:,0]
+    P_y = P[:,1]
+    P_z = P[:,2]
+    L_x = wireL[:,0]
+    L_y = wireL[:,1]
+    L_z = wireL[:,2]
+    h_x = P_z*L_y - P_y*L_z
+    h_y = -(P_z*L_x - P_x*L_z)
+    h_z = P_y*L_x - P_x*L_y
+    a = L_x**2 + L_y**2 + L_z**2
+    b = L_x*P_x + L_y*P_y + L_z*P_z
+    c = P_x**2 + P_y**2 + P_z**2
+    t_i, t_f = np.float64(-0.5), np.float64(0.5)
+    integral_f = constant * (b - a*t_f)/((b**2 - a*c)(t_f*(a*t_f - 2*b)+c)**(0.5))
+    integral_i = constant * (b - a*t_i)/((b**2 - a*c)(t_i*(a*t_i - 2*b)+c)**(0.5))
+    integralSolved = integral_f- integral_i
+    preOut = np.empty([N,3] dtype=float64)
+    preOut[:,0] = h_x*integralSolved
+    preOut[:,1] = h_y*integralSolved
+    preOut[:,2] = h_z*integralSolved
+    out = np.sum(preOut, axis=0, ndmin=2)
+    return out
+
+def circleLoop(radius, orientVector, N, I):
+    """
+    Inputs-
+
+    radius:              np.float64          Radius in meters
+    orientVector:   (n,3)np.float64          Unit vector of loop axis
+    N:                   int                 Number of wire segments
+    I:                   np.float64          Current
+    -------------
+    Return-
+
+    positionVectors, lengthVectors
+
+    positionVectors:     (n,3)np.float64    center of wire 
+    lengthVectors:       (n,3)np.float64    length vector
+    ------------
+    
+    """
+    return
